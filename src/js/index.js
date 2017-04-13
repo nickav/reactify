e6948454b4cb17ddfb2d59753c5a29fda8e97b0b
Nick Aversano <nick23hi@gmail.com>
2017-04-12 17:50:19 -0700

    move around files, etc

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './components/App'
 import { AppContainer } from 'react-hot-loader'
+import App from './components/App'
 import store from './store'
 
 const render = Component => {
