e0af1163454eb8147efd197aa31d6af95a1dfb95
Nick Aversano <nick23hi@gmail.com>
2017-04-12 17:14:21 -0700

    use axios for request handling

--- src/js/dollar.js
+++ src/js/dollar.js
@@ -1,84 +1,17 @@
-import 'whatwg-fetch'
+import axios from 'axios'
+import queryString from 'query-string'
 
-/**
- * Thin wrapper around fetch for RESTful JSON actions.
- */
-
-/** Default request options. */
-const defaultOptions = {
-  credentials: 'same-origin',
+const $ = axios.create({
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
-  },
-}
-
-/** Ensures JSON is a string. */
-const stringify = json => typeof json == 'string' ? json : JSON.stringify(json)
-
-/**
- * fetch with JSON defaults (as specified in defaultOptions).
- * throws response on a non-200 status code
- * e.g.
-    fetchJson('http://hello.world/api.json')
-      .then(result => {
-        console.log(result.status)
-      })
-      .catch(err => {
-        console.error(err)
-      })
- */
-export const fetchJson = (url, options) => {
-  options = Object.assign({}, defaultOptions, options)
-  return fetch(url, options)
-    .then(response => {
-      // forward response or throw error
-      if (response.status >= 200 && response.status < 300) {
-        return response
-      }
-      throw response
-    })
-    .then(response => response.json())
-}
-
-export const get = (url, options) => {
-  options = Object.assign({}, options, { method: 'GET' })
-  return fetchJson(url, options)
-}
-
-export const post = (url, options) => {
-  options = Object.assign({}, options, { method: 'POST' })
-  if (options.body) {
-    options.body = stringify(options.body)
   }
-  return fetchJson(url, options)
-}
+})
 
-export const put = (url, options) => {
-  options = Object.assign({}, options, { method: 'PUT' })
-  options.body = stringify(options.body)
-  return fetchJson(url, options)
-}
-
-export const patch = (url, options) => {
-  options = Object.assign({}, options, { method: 'PATCH' })
-  options.body = stringify(options.body)
-  return fetchJson(url, options)
-}
-
-export const deleet = (url, options) => {
-  options = Object.assign({}, options, { method: 'DELETE' })
-  return fetchJson(url, options)
-}
-
-/** jQuery-esque object. */
-const $ = {
-  fetch: fetchJson,
-  get,
-  post,
-  put,
-  patch,
-  delete: deleet
+const $_get = $.get
+$.get = (url, data, config) => {
+  url += '?' + queryString.stringify(data, { arrayFormat: 'bracket' })
+  return $_get(url, config)
 }
 
 export default $
